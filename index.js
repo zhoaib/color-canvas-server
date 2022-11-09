3a2b6a852816bee47b5852ba0f38b7a981d01283
zhoaib <zhoaibhassan@gmail.com>
2022-11-10 03:36:43 +0600

    review api

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('colorCanvas').collection('services');
+        const reviewCollection = client.db('colorCanvas').collection('reviews');
 
         app.get('/services', async (req, res) => {
             const query = {}
@@ -36,7 +37,26 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        });
+
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
         })
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
+
+        });
 
     }
     finally {
