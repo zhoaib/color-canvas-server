a132e4754c0a622ee041ee1a14b09c84fd10bec6
zhoaib <zhoaibhassan@gmail.com>
2022-11-10 06:09:32 +0600

    delete

--- index.js
+++ index.js
@@ -58,6 +58,26 @@ async function run() {
 
         });
 
+        app.patch('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status
+            const query = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    status: message
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
