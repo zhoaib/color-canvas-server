e5ca459fe2f5e0cdab8a0f7958d98f2cc11d83ed
zhoaib <zhoaibhassan@gmail.com>
2022-11-10 15:35:08 +0600

    token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
